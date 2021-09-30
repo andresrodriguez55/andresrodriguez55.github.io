e417bb6de078ce7a9f03db2a741a0fa1b6de04c3
andresrodriguez55 <57767201+andresrodriguez55@users.noreply.github.com>
2021-09-30 04:05:45 +0300

    Updates

--- Backend/SQL/Queries.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM POST;
-
-SELECT * FROM CATEGORY;
-
-SELECT * FROM USER_COMMENT;
-
-SELECT ID FROM USER_COMMENT ORDER BY ID DESC LIMIT 0;
-
-SELECT table_schema AS "Database", SUM(data_length + index_length) / 1024 / 1024 AS "Size (MB)" FROM information_schema.TABLES GROUP BY table_schema;
