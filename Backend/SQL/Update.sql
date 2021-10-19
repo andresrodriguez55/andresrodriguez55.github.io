aedba8f8bdf943122ccc10bcd720314286c11ab3
Andres Arturo Rodriguez Calderon <57767201+andresrodriguez55@users.noreply.github.com>
2021-10-20 00:30:18 +0300

    Add files via upload

---
+++ Backend/SQL/Update.sql
@@ -0,0 +1,9 @@
+UPDATE POST SET Content="
+" WHERE ID=-1;
+
+update USER_COMMENT SET CommentDate="2021.09.27 20:01" where CommentDate="2021.09.29 02:33";
+
+UPDATE CATEGORY SET NameOfCategory="..." WHERE NameOfCategory='...'
+;
+
+UPDATE POST SET CATEGORY=NULL WHERE ID=-1; 
